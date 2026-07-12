6556b4a01f5b4db8bc75d4f7f1b9b16922f2c457
NCBarrett <80253863+NCBarrett@users.noreply.github.com>
2026-07-12 12:14:56 -0700

    Cleaned up layout and have wired most controls

--- src/main/java/org/example/listandcomparepics/Controller.java
+++ src/main/java/org/example/listandcomparepics/Controller.java
@@ -23,7 +23,6 @@ public class Controller {
     @FXML public Button dirBrowser;
     @FXML public Button submitBtn;
 
-    @FXML public HBox root;
     @FXML public HBox renameBtnBox;
     @FXML public HBox moveBtnBox;
 
@@ -35,6 +34,9 @@ public class Controller {
     @FXML public ListView<String> fileListView = new ListView<>();
     @FXML public ListView<String> rtFileListView = new ListView<>();
 
+    @FXML public RadioButton renameBtn = new RadioButton("Rename: ");
+    @FXML public RadioButton moveBtn = new RadioButton("Move: ");
+
     @FXML public StackPane imageContainer;
     @FXML public StackPane rtImageContainer;
 
@@ -43,11 +45,13 @@ public class Controller {
     @FXML public TextField dirName;
     @FXML public TextField rtTextRegEx;
 
-    @FXML public ToggleGroup actionChoiceGroup;
+    @FXML public ToggleGroup actionChoiceGrp = new ToggleGroup();
 
     @FXML public VBox leftPane;
     @FXML public VBox choiceGrpContainer;
     @FXML public VBox rightPane;
+    @FXML public VBox root;
+    @FXML public VBox dirPane;
 
     private DirectoryWatcherService watcherService;
     private DirectoryListingService listingService;
@@ -63,7 +67,7 @@ public class Controller {
                 (observable, oldValue,
                  newValue) -> {
                     if (newValue != null) {
-                        loadImage(newValue);
+                        loadImage(newValue, imageViewer);
                         fileName.setText(newValue);
                         choiceGrpContainer.setDisable(false);
                     } else  {
@@ -71,13 +75,53 @@ public class Controller {
                     }
         });
 
+        rtFileListView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue,
+                 newValue) -> {
+                    if (newValue != null) {
+                        loadImage(newValue, rtImageViewer);
+                    }
+                });
+
         textRegEx.textProperty().addListener((
                 observable, oldValue,
-                newValue)
-                -> {refreshListView();
+                newValue) -> {refreshListView();
         });
 
+        rtTextRegEx.textProperty().addListener((
+                observable, oldValue,
+                newValue) -> { refreshListView();
+        });
+
+        renameBtn.setToggleGroup(actionChoiceGrp);
+        moveBtn.setToggleGroup(actionChoiceGrp);
+
+        actionChoiceGrp.selectedToggleProperty().addListener(
+                (observable, oldToggle,
+                 newToggle) -> {
+                    if (newToggle != null) {
+                        RadioButton selRadio =  (RadioButton) newToggle;
+                        String selText = selRadio.getText();
+                        System.out.println("radioBtn = " + selText);
+
+                        if (selText.equals("Rename: ")) {
+                            System.out.println("Rename");
+                            fileName.setDisable(false);
+                            dirName.setDisable(true);
+                        } else {
+                            System.out.println("Move to");
+                            dirName.setDisable(false);
+                            fileName.setDisable(true);
+                        }
+                        submitBtn.setDisable(false);
+                    }
+                }
+        );
+
         choiceGrpContainer.setDisable(true);
+        fileName.setDisable(true);
+        dirName.setDisable(true);
+        submitBtn.setDisable(true);
     }
 
     public void setStage(Stage stage) {
@@ -114,6 +158,9 @@ public class Controller {
             /// Update the ListView items
             fileListView.setItems(listingService.getDirectoryListing(
                     currentWatchDir, textRegEx.getText()));
+            rtFileListView.setItems(listingService.getDirectoryListing(
+                    currentWatchDir, rtTextRegEx.getText()
+            ));
 //            resizeListViewToContent();
         }
     }
@@ -122,7 +169,7 @@ public class Controller {
         watcherService.stopWatching();
     }
 
-    private void loadImage(String filename) {
+    private void loadImage(String filename, ImageView targetView) {
         /// Suggested by IDE
         File imageFile = new File(currentWatchDir.toString(), filename);
         System.out.println("Path = " + imageFile.getAbsolutePath());
@@ -131,28 +178,10 @@ public class Controller {
             /// Load the image securely using getResourceAsStream
             Image image = new Image(imageFile.toURI().toString());
             /// imageViewer.setImage(image); DOES NOT bind the image
-            imageViewer.imageProperty().set(image);
+            targetView.imageProperty().set(image);
         } catch (Exception e) {
             System.err.println("Error loading image: " + filename);
-            imageViewer.imageProperty().set(null);
+            targetView.imageProperty().set(null);
         }
     }
 }
-
-//    private void resizeListViewToContent() {
-//        double maxWidth = 0;
-//        Font font = Font.getDefault();
-//
-//        for (String item : fileListView.getItems()) {
-//            Text text = new Text(item);
-//            text.setFont(font);
-//            double width = text.getLayoutBounds().getWidth();
-//            if (width > maxWidth) {
-//                maxWidth = width;
-//            }
-//        }
-//
-////        System.out.println("maxWidth: " + maxWidth);
-//        fileListView.setPrefWidth(300); // 400
-//
-//    }
